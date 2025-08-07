054e0cd164b46c6e0f30eb1223e02323d83cb173
Melkizedeki <mmagota2@gmail.com>
2025-08-07 21:05:39 +0300

    Implement JWT authentication entry point for handling unauthorized access

--- src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
+++ src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
@@ -23,7 +23,7 @@ public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
     @Value("${spring.app.expiration-ms}")
-    private int jwtExpirationMs;
+    private long jwtExpirationMs;
 
     @Value("${spring.app.secret}")
     private String jwtSecret;

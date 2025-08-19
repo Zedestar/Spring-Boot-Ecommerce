d0c2e0a96ef7ff189522ca90a499c53c2935d5bc
Melkizedeki <mmagota2@gmail.com>
2025-08-19 20:18:04 +0300

    Adding the controller endpoint for getting all the list of carts in DTO form

--- src/main/java/com/ecommerce/project/controller/CartController.java
+++ src/main/java/com/ecommerce/project/controller/CartController.java
@@ -7,10 +7,9 @@ import com.ecommerce.project.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -23,5 +22,10 @@ public class CartController {
     public ResponseEntity<CartDTO> addProductToCart(@PathVariable Long productId, @PathVariable Integer quantity){
 //        CartDTO cartDTO = cartService.addProductsToCart(productId, quantity);
         return ResponseEntity.status(HttpStatus.CREATED).body(cartService.addProductsToCart(productId, quantity));
+    }
+
+    @GetMapping("/admin/cart/list")
+    public ResponseEntity<List<CartDTO>> gettingAllCartList(){
+        return ResponseEntity.ok().body(cartService.allCartList());
     }
 }
